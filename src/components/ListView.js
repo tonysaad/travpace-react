41324fb446102573641e55724d2e25e10a8ffdd7
Omar Essam <o.essameldin@gmail.com>
2017-07-05 17:41:02 +0200

    initial api request

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import TripWidget from './TripWidget';
+import axios from 'axios';
+
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
-var list =[
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-    <TripWidget/>,
-];
+
 
 class ListView extends Component {
+  constructor(){
+    super();
+    this.state={
+      list:[]
+    }
+    this.url = "http://localhost/mitchdesigns/travpace/wp-json/v1/hotel-deals-and-packages";
+  }
+    updateState = (list) => {
+        console.log(list);
+        this.setState({list:list});
+    }
+  componentDidMount(){
+    this.getList(["type=hotel-deal"]);
+  }  
+  getList = (filters) => {
+    let tempURL = this.url;
+    filters.forEach((filter, i)=>{
+      tempURL += (i == 0)? "?"+filter : "&"+filter;
+    });
+    axios.get(tempURL).then((response) => {
+      if(response.data.status == "success"){
+        console.log("hena");
+        this.updateState(response.data.data);
+      }
+    });
+  }
   render() {
-    return (
-      <div className="list-view">
+    let list = this.state.list.map((item)=>{
+        return(
+        <TripWidget/>
+        )
+    });
+    return(
+      <div className='list-view'>
         {list}
       </div>
-    );
+    )
   }
 }
 
