49dec586552074da3beea5e7fd4618e989719080
oja <georgesaad111@gmail.com>
2017-07-18 13:11:49 +0200

    add files

--- src/components/booking-page/ChooseDate.js
+++ src/components/booking-page/ChooseDate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 import './booking-page.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 class ChooseDate extends Component {
   render() {
@@ -10,7 +12,13 @@ class ChooseDate extends Component {
             <h3>أختار تاريخ السفر</h3>
           </div>
           <div className="booking-text-content">
-
+<DateRangePicker
+  startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+  endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+  onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+  focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+  onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+/>
           </div>
         </div>
 
