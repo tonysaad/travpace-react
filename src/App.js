c9633b499d983a54449706267bc048cf85721daa
Omar Essam <omaressameldin@localhost.localdomain>
2017-07-20 11:56:36 +0200

    stars filter meals filter and types filter finished long and short trip still has a bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 // import TravelIncluded from './components/TravelIncluded';
 import Header from './components/Header';
 import ListPaginationWrapper from './components/list-pagination-wrapper/ListPaginationWrapper';
-import Filter from './components/Filter';
 // import QRHeader from './components/QRHeader';
 // import Label from './components/Label';
 // import ListPrices from './components/ListPrices';
@@ -28,31 +27,18 @@ const App = () => (
 
   <Router>
     <div>
-      <Header/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/:id" component={(match) => <SinglePage params={match.match.params}/>} />
-      <Route exact path="/book/:id" component={(match) => <Book params={match.match.params}/>} />
-      <Route exact path="/thanks" component={Thanks}/>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/:id" component={(match) => <SinglePage params={match.match.params} />} />
+      <Route exact path="/book/:id" component={(match) => <Book params={match.match.params} />} />
+      <Route exact path="/thanks" component={Thanks} />
     </div>
   </Router>
 )
 
 const Home = () => (
   <div className="HomePage">
-    <div className="container">
-      <div className="right-side">
-        <div className="sponsored">
-          <span>برعاية</span>
-          <img src="/img/right-sponsored.png" alt="sponsored"/>
-        </div>
-        <Filter/>
-      </div>
-      <ListPaginationWrapper history={history} />
-      <div className="sponsored middle">
-        <span>برعاية</span>
-        <img src="/img/middle-sponsored.png" alt="sponsored"/>
-      </div>
-    </div>
+    <ListPaginationWrapper history={history} />
   </div>
 )
 
--- src/components/list-pagination-wrapper/ListPaginationWrapper.js
+++ src/components/list-pagination-wrapper/ListPaginationWrapper.js
@@ -4,76 +4,113 @@ import ListView from '../ListView';
 import Pagination from '../Pagination';
 import TitleCity from '../TitleCity';
 import SingleItem from '../single-item/SingleItem';
+import Filter from '../Filter';
 
 class ListPaginationWrapper extends Component {
   constructor(props) {
-      super(props);
+    super(props);
     this.state = {
       list: [],
-      filters:  {"_page":1, "_limit": 16},
-      totalCount:0,
+      filters: { "_page": 1, "_limit": 16 },
+      totalCount: 0,
       popupToggle: false,
       // popupData: '',
-      popupIndex:-1
+      popupIndex: -1
     };
     this.url = "http://thebackendcrew.com:8888/deals-and-packages";
   }
   updateState = (list, filters, totalCount) => {
-    this.setState({ list, filters, totalCount  });
+    this.setState({ list, filters, totalCount });
   };
-  changePage = (page) =>{
-    this.getList({"_page":page});
+  changePage = (page) => {
+    this.getList({ "_page": page });
   }
-switchPopup=(direction) =>{
-  if (this.state.popupIndex +1 < this.state.list.length && direction === 'left') {
-    this.props.history.replace("/"+this.state.list[this.state.popupIndex +1].id);
-    this.setState({ popupIndex: this.state.popupIndex +1})
-  }
-  if (this.state.popupIndex -1 >= 0 && direction === 'right') {
-    this.props.history.replace("/"+this.state.list[this.state.popupIndex -1].id);
-    this.setState({ popupIndex: this.state.popupIndex -1})
-  }
-};
-  populateAndTogglePopup = (popupToggle, popupIndex = -1) =>{
-    // console.log(popupData);
-    console.log('popupIndex');
-    console.log(popupIndex);
+  switchPopup = (direction) => {
+    if (this.state.popupIndex + 1 < this.state.list.length && direction === 'left') {
+      this.props.history.replace("/" + this.state.list[this.state.popupIndex + 1].id);
+      this.setState({ popupIndex: this.state.popupIndex + 1 })
+    }
+    if (this.state.popupIndex - 1 >= 0 && direction === 'right') {
+      this.props.history.replace("/" + this.state.list[this.state.popupIndex - 1].id);
+      this.setState({ popupIndex: this.state.popupIndex - 1 })
+    }
+  };
+  populateAndTogglePopup = (popupToggle, popupIndex = -1) => {
     document.body.classList.toggle('no-scroll', popupToggle)
-    if(popupToggle === true)
-      this.props.history.push("/"+this.state.list[popupIndex].id);
-      
-      else
+    if (popupToggle === true)
+      this.props.history.push("/" + this.state.list[popupIndex].id);
+
+    else
       this.props.history.goBack();
-    this.setState({popupToggle, popupIndex})
+    this.setState({ popupToggle, popupIndex })
   }
 
-    componentDidMount() {
+  componentDidMount() {
     this.getList({});
   }
 
   getList = (newFilters) => {
-    let allFilters = {...this.state.filters};
+    let allFilters = { ...this.state.filters };
     for (let key in newFilters)
-        allFilters[key] = newFilters[key];
+      allFilters[key] = newFilters[key];
     let tempURL = this.url;
     Object.keys(allFilters).forEach((filterKey, i) => {
-      let filter = filterKey+"="+allFilters[filterKey];
+      let filter = ""
+      if (Array.isArray(allFilters[filterKey])) {
+        allFilters[filterKey].forEach((f, i) => {
+          filter += (i === 0) ? filterKey + "=" + f : "&" + filterKey + "=" + f
+        })
+      }
+      else
+        filter = filterKey + "=" + allFilters[filterKey];
       tempURL += i === 0 ? "?" + filter : "&" + filter;
     });
-    axios.get(tempURL).then(response => { 
+    console.log(tempURL);
+    axios.get(tempURL).then(response => {
       this.updateState(response.data, allFilters, response.headers["x-total-count"]);
     });
   };
 
-  render(){
-    let popup = (this.state.popupToggle)? (<SingleItem info = {this.state.list[this.state.popupIndex]} popupFunction = {this.populateAndTogglePopup} switchPopup={this.switchPopup}/>) : '';
-    return(    
-      <div className="left-side">
-        <TitleCity/>
-        <ListView popupFunction = {this.populateAndTogglePopup} list = {this.state.list}/>
-        <Pagination totalItems= {this.state.totalCount} changePage = {this.changePage}/>
-        {popup}
+  //   getList = (newFilters) => {
+  //   let allFilters = { ...this.state.filters };
+  //   for (let key in newFilters)
+  //     allFilters[key] = newFilters[key];
+  //   let tempURL = this.url;
+  //   Object.keys(allFilters).forEach((filterKey, i) => {
+  //     let filter = filterKey + "=" + allFilters[filterKey];
+  //     console.log(allFilters[filterKey])
+  //     tempURL += i === 0 ? "?" + filter : "&" + filter;
+  //   });
+
+  //   axios.get(tempURL).then(response => {
+  //     this.updateState(response.data, allFilters, response.headers["x-total-count"]);
+  //   });
+  // };
+
+
+  render() {
+    let popup = (this.state.popupToggle) ? (<SingleItem info={this.state.list[this.state.popupIndex]} popupFunction={this.populateAndTogglePopup} switchPopup={this.switchPopup} />) : '';
+    return (
+      <div className="container">
+        <div className="right-side">
+          <div className="sponsored">
+            <span>برعاية</span>
+            <img src="/img/right-sponsored.png" alt="sponsored" />
+          </div>
+          <Filter updateFilters={this.getList} />
+        </div>
+        <div className="left-side">
+          <TitleCity />
+          <ListView popupFunction={this.populateAndTogglePopup} list={this.state.list} />
+          <Pagination totalItems={this.state.totalCount} changePage={this.changePage} />
+          {popup}
+        </div>
+        <div className="sponsored middle">
+          <span>برعاية</span>
+          <img src="/img/middle-sponsored.png" alt="sponsored" />
+        </div>
       </div>
+
     )
   }
 }
