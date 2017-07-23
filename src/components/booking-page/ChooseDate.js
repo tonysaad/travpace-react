578266a737199725a9ff06294c06a5e9e3e7df76
Tony Saad <tony@wego.com>
2017-07-23 20:30:08 +0200

    Adding Date Range Picker Todos: Make sure user can't select a package that includes disabled days Add marks for "Sold Out" packages Style the green/light packages

--- src/components/booking-page/ChooseDate.js
+++ src/components/booking-page/ChooseDate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
 import './booking-page.css';
 import 'react-dates/lib/css/_datepicker.css';
 
@@ -7,27 +9,43 @@ class ChooseDate extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      from: null,
+      to: null,
     };
   }
+  handleDayClick = from => {
+    const from2 = new Date(from);
+    from2.setDate(from2.getDate() + this.props.days-1);
+    this.setState({
+      from,
+      to: from2,
+    });
+  };
   render() {
+    const { from, to } = this.state;
     return (
-
       <div className="ChooseDate">
         <div className="booking-small-head">
           <h3>أختار تاريخ السفر</h3>
         </div>
         <div className="booking-text-content">
-          <DayPickerRangeController
-            onDatesChange={this.onDatesChange}
-            onFocusChange={this.onFocusChange}
-            focusedInput={this.state.focusedInput}
-            startDate={this.state.startDate}
-            endDate={this.state.endDate}
-          />
+          <div className="dateRange">
+            <DayPicker
+              numberOfMonths={2}
+              selectedDays={[from, { from, to }]}
+              onDayClick={this.handleDayClick}
+              disabledDays={[
+                new Date(2017, 7, 12),
+                new Date(2017, 7, 13),
+                {
+                  after: new Date(2017, 8, 11),
+                  before: new Date(2017, 8, 15),
+                },
+              ]}
+            />
+          </div>
         </div>
       </div>
-
     );
   }
 }
