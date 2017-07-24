e29e7cd7a44536f41c0e4746fb6240bb4eaac268
oja <georgesaad111@gmail.com>
2017-07-24 13:02:05 +0200

    style calender

--- src/components/booking-page/ChooseDate.js
+++ src/components/booking-page/ChooseDate.js
@@ -3,7 +3,6 @@ import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
 import './booking-page.css';
-import 'react-dates/lib/css/_datepicker.css';
 
 class ChooseDate extends Component {
   constructor(props) {
