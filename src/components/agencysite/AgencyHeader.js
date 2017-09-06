7bbdcf0ba993c112c8c235ee86affe5b63cc2760
oja <georgesaad111@gmail.com>
2017-09-06 16:37:40 +0200

    add files

---
+++ src/components/agencysite/AgencyHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './agencyheader.css';
+
+class AgencyHeader extends Component {
+    render(){ 
+        return (
+            <div className="AgencyHeader">
+            </div>
+        );
+    }
+}
+
+export default AgencyHeader;
